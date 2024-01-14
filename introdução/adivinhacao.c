e7e74b4aa273e74357b1fee347b25c0b89261458
GuilhermePortella <guilhermeportella2@gmail.com>
2024-01-13 21:59:43 -0300

    modified:   "introdu\303\247\303\243o/adivinhacao.c"

--- introdução/adivinhacao.c
+++ introdução/adivinhacao.c
@@ -2,9 +2,14 @@
 
 int main(){
 
+    //inicio do programa - cabecalho
     printf("**********************************\n");
     printf("*Bem vindo ao jogo de adivinhação*\n");
     printf("**********************************\n");
 
+    //declarar variavel int e imprimir essa variavel em um printf
+    int numerosecreto = 42 ;
+
+    //printf("adivinhe o %d numero secreto"\n, numerosecreto);
 
 }
