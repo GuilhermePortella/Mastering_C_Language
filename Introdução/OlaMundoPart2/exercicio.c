aef4edef96461a5fe119a470d7c9f256e85dea70
GuilhermePortella <guilhermeportella2@gmail.com>
2024-01-15 20:26:21 -0300

    new file:   "Introdu\303\247\303\243o/OlaMundoPart2/exercicio.c" 	new file:   "Introdu\303\247\303\243o/OlaMundoPart2/exercicio2.c"

---
+++ Introdução/OlaMundoPart2/exercicio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 15;
+
+    if (numero > 20)
+    {
+        printf("numero grande");
+    }
+    else
+    {
+        printf("numero pequeno");
+    }
+}
