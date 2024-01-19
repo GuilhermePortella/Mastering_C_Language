f527a01240660c8346d2f83a3e7a8b213b49c1b7
GuilhermePortella <guilhermeportella2@gmail.com>
2024-01-18 23:42:18 -0300

    iniciando for

---
+++ Introdução/OlaMundoPart2/adivinhacao3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    //inicio do programa - cabecalho
+    printf("**********************************\n");
+    printf("*Bem vindo ao jogo de adivinhação*\n");
+    printf("**********************************\n");
+
+    //declarar variavel int 
+    int numerosecreto = 42 ;
+    int chute;
+
+    for(int i = 1; i<= 3; i++){
+    printf("Qual e seu chute? \n");
+    //função (scanf) para ler o input do usuario
+    scanf("%d", &chute);
+    printf("Seu chute foi %d \n", chute);  
+
+    //comparacao se chute e igual numero secreto
+    int acertou = (chute == numerosecreto);
+
+    if(acertou){
+        printf("Voce acertou! \n");
+    }else{
+        int maior = (chute > numerosecreto);
+        if (maior){
+            printf("Seu chute foi maior que o numero secreto! \n");
+        }else {
+            printf("Seu chute foi menor que o numero secreto! \n");
+        }
+    }
+}
+    printf("Fim de jogo \n");
+}
