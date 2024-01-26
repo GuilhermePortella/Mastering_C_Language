9eded0e3027ef375b76e19a32bfb49cbef84d7bc
GuilhermePortella <guilhermeportella2@gmail.com>
2024-01-25 23:50:52 -0300

    Iniciando rand()

--- Introdução/OlaMundoPart3/convertendoVariaveis.c
+++ Introdução/OlaMundoPart3/convertendoVariaveis.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
+    int n1 = rand();
+    int n2 = rand();
     int a = 3;
     int b = 2;
 
     double portos = (double)a / (double)b;
-    printf("%.3f\n", portos);
+    //printf("%.3f\n", portos);
     
     double pi = 3.1415;
     int piConversion = (int)pi;
-    printf("%f  %d\n", piConversion);
+    //printf("%f  %d\n", piConversion);
+    printf("%d  %d\n", n1,n2);
 
 }
